791fcd1064a6a41523acb064190bc9500ff0c40c
fevse <fedulov.v.s@gmail.com>
2023-01-16 18:51:08 +0600

    update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -17,11 +17,11 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
     
-
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
@@ -44,7 +44,6 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ["due_back"]
 
-
     def __str__(self):
         return f'{self.id} ({self.book.title})'
 
@@ -58,9 +57,12 @@ class Author(models.Model):
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
 
-
     def __str__(self):
         return '{self.last_name}, {self.first_name}'
 
 
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='The language in which the book is written')
 
+    def __str__(self):
+        return self.name
