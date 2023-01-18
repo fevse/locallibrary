7b9b7a3e99dd926de5aed8634164b346bab11f86
fevse <fedulov.v.s@gmail.com>
2023-01-18 18:01:17 +0600

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre, Language
 
 # Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_languages = Language.objects.count()
+    num_books_letter_o = Book.objects.filter(title__icontains='о').count()
+
+
+    return render(
+        request,
+        'index.html',
+        context=
+        {'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_languages': num_languages,
+        'num_books_letter_o': num_books_letter_o},
+    )
