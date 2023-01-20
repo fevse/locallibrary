d9665cc964c4f9b697aa93f457582cccc502cc18
fevse <fedulov.v.s@gmail.com>
2023-01-20 19:16:18 +0600

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -34,3 +34,12 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 3
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
