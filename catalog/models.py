9311d1f705102e57dbd91bcaa53f7a19f1bac292
fevse <fedulov.v.s@gmail.com>
2023-01-24 21:26:10 +0600

    update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -54,7 +54,6 @@ class BookInstance(models.Model):
             return True
         return False
 
-
     class Meta:
         ordering = ["due_back"]
         permissions = (("can_mark_returned", "Set book as returned"),)
