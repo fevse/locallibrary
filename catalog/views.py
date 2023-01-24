70c13263fb6681f5e8dab9236ff42af57658a690
fevse <fedulov.v.s@gmail.com>
2023-01-24 21:29:01 +0600

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 def index(request):
@@ -55,3 +55,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class BorrowedBookListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name ='catalog/borrowed_book_list.html'
+    paginate_by = 10
+    permission_required = 'catalog.can_mark_returned'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
