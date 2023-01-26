95323ddf4a910d69fb807319d0521aea1448c3ee
fevse <fedulov.v.s@gmail.com>
2023-01-26 22:13:50 +0600

    update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed_books/', views.BorrowedBookListView.as_view(), name='borrowed-books')
+    path('borrowed_books/', views.BorrowedBookListView.as_view(), name='borrowed-books'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
