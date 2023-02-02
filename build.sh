2d63a3af5b4512669e1f95db6479132ad1d022ee
fevse <fedulov.v.s@gmail.com>
2023-02-02 19:59:58 +0600

    update dependencies

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install setuptools
 poetry install
-pip install setuptools
-poetry install setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
