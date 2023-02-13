2275d5b7f8128e2d2867d553bd732e12eff695cd
fevse <fedulov.v.s@gmail.com>
2023-02-13 17:03:15 +0600

    update build

--- build.sh
+++ build.sh
@@ -2,20 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install setuptools
-pip update setuptools
-poetry add setuptools
-poetry update setuptools
 poetry install
-pip install setuptools
-pip update setuptools
-poetry add setuptools
-poetry update setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-pip install setuptools
-pip update setuptools
-poetry add setuptools
-poetry update setuptools
